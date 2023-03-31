e2eab802a55253d9cf12338eee5404b424cf4089
Thomas Rodewald <Thomas.Rodewald@Cassel.de>
2023-03-31 21:32:02 +0200

    - add some error handling - add selection of used model - translations (work in pprogress)

--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+# pip3 install openai python-telegram-bot
+
 import os
 import sys
 import json
 import openai
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, error
 from telegram.ext import (
     Application,
     ContextTypes,
@@ -55,9 +57,15 @@ class User:
 class Config:
     def __init__(self):
         config = self._loadFile()
-        self.openai_key = config['opeanai_key']
+        self.openai_key = config['openai_key']
         self.telegram_token = config['telegram_token']
+        self.language = config['language']
         self.max_history_entries = config['max_history_entries']
+        models = []
+        for model in config['models']:
+            models.append(model)
+        self.models = set(models)
+        self.current_model = config['current_model']
         self.users = []
         for user in config['users']:
             if user:
@@ -91,25 +99,48 @@ class Config:
             print("config.json: decode error!")
             sys.exit()
 
+    def saveCurrentModel(self) -> None:
+        data = self._loadFile()
+        if data['current_model'] != self.current_model:
+            data['current_model'] = self.current_model
+            configFile = open("config.json", "w")
+            configFile.write(json.dumps(data))
+            configFile.close()
+
+# -------------------------------------------------------------------------------------
+
+class Translations:
+    pass
+
 # -------------------------------------------------------------------------------------
 
 class OpenaAI_API:
-    def __init__(self, key: str) -> None:
-        openai.api_key = key
-        self.model = "gpt-3.5-turbo"
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        openai.api_key = config.openai_key
         self.query = [{"role": "user", "content": ""}]
 
     def setModel(self, new_model: str) -> None:
         models = openai.Model.list()
         if new_model in models['data']:
-            self.model = new_model
+            self.config.current_model = new_model
+            self.config.saveCurrentModel()
 
     def setQueryText(self, text: str) -> None:
         self.query[0]['content'] = text
 
     def getResponse(self) -> str:
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.query)
+        completion = openai.ChatCompletion.create(model=self.config.current_model, messages=self.query)
         return completion.choices[0].message.content
+    
+    def getAvailableModels(self):
+        models = openai.Model.list()
+        models_list = []
+        for model in models['data']:
+            models_list.append(model.id)
+        models_set = set(models_list)
+        available_models = models_set.intersection(config.models)
+        return available_models
 
 # -------------------------------------------------------------------------------------
 
@@ -150,6 +181,20 @@ class ChatGPTBot:
             }, fallbacks=[CommandHandler("cancel", self.cancel)])
         self.updater.add_handler(topic_handler)
 
+        self.MODELSELECT, self.MODELSELECTED = range(2)
+        model_handler = ConversationHandler(
+            entry_points = [CommandHandler("model", self.model)],
+            states = {
+                self.MODELSELECT: [
+                    MessageHandler(filters.Regex("^Modell wählen$"), self.setmodel),
+                    MessageHandler(filters.Regex("^zeige aktuelles Modell$"), self.showmodel)
+                ],
+                self.MODELSELECTED: [
+                    MessageHandler(filters.Regex(".*"), self.setnewmodel)
+                ]
+            }, fallbacks=[CommandHandler("cancel", self.cancel)])
+        self.updater.add_handler(model_handler)
+
         self.CHAT = range(1)
         chat_handler = ConversationHandler(
             entry_points = [CommandHandler('chat', self.chat_query)],
@@ -169,24 +214,34 @@ class ChatGPTBot:
             reply_keyboard.append(["zeige aktuelles Thema"])
             reply_keyboard.append(["lösche Thema"])
             reply_keyboard.append(["abbrechen"])
-            await update.message.reply_text("Wie kann ich helfen?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+            try:
+                await update.message.reply_text("Wie kann ich helfen?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+            except error.NetworkError:
+                self.updater = Application.builder().token(self.config.telegram_token).build()
+                return self.topic(update, context)
         return self.SELECTION
         
     async def newtopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        await update.message.reply_text("not implemented yet")
-        await update.message.reply_text("Thema?\nMöglichst als ein Wort!")
+        try:
+            await update.message.reply_text("Thema?\nMöglichst als ein Wort!", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.newtopic(update, context)
         return self.NEWTOPIC
     
     async def newtopicname(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         user = self.userById(update)
         user.data['current_topic'] = update.message.text
-        if update.message.text in user.topics():
-            await update.message.reply_text("Thema existiert schon und ist jetzt aktiv!")
-        else:
-            user.data['topics'].append({"name": update.message.text.strip(), "history": []})
-            user.data
-        user.save()
-        await update.message.reply_text("OK")
+        try:
+            if update.message.text in user.topics():
+                await update.message.reply_text("Thema existiert schon und ist jetzt aktiv!", reply_markup= ReplyKeyboardRemove())
+            else:
+                user.data['topics'].append({"name": update.message.text.strip(), "history": []})
+                user.save()
+                await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.newtopicname(update, context)
         return ConversationHandler.END
 
     async def existingtopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -195,29 +250,45 @@ class ChatGPTBot:
         topics = user.topics()
         for topic in topics:
             reply_keyboard.append([topic])
-        await update.message.reply_text("Deine Themen:", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+        try:
+            await update.message.reply_text("Deine Themen:", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.existingtopic(update, context)
         return self.TOPICSELECTION
 
     async def setselectedtopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         user = self.userById(update)
         user.data['current_topic'] = update.message.text
         user.save()
-        await update.message.reply_text("OK")
+        try:
+            await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.setselectedtopic(update, context)
         return ConversationHandler.END
 
     async def cleartopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         user = self.userById(update)
         user.data['current_topic'] = ""
         user.save()
-        await update.message.reply_text("OK")
+        try:
+            await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.cleartopic(update, context)
         return ConversationHandler.END
     
     async def currenttopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         user = self.userById(update)
-        if user.hasActiveTopic():
-            await update.message.reply_text("aktuelles Thema:\n{}".format(user.data['current_topic']))    
-        else:
-            await update.message.reply_text("aktuelles Thema:\n keines")
+        try:
+            if user.hasActiveTopic():
+                await update.message.reply_text("aktuelles Thema:\n{}".format(user.data['current_topic']), reply_markup= ReplyKeyboardRemove())    
+            else:
+                await update.message.reply_text("aktuelles Thema:\n keines", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.currenttopic(update, context)
         return ConversationHandler.END
 
     async def deletetopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -226,7 +297,11 @@ class ChatGPTBot:
         topics = user.topics()
         for topic in topics:
             reply_keyboard.append([topic])
-        await update.message.reply_text("Deine Themen:", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+        try:
+            await update.message.reply_text("Deine Themen:", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.deletetopic(update, context)
         return self.TOPICSELECTION_DELETE
     
     async def deleteselectedtopic(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -240,15 +315,118 @@ class ChatGPTBot:
             else:
                 counter += 1
         user.save()
-        await update.message.reply_text("OK")
+        try:
+            await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.deleteselectedtopic(update, context)
         return ConversationHandler.END
+    
+    async def model(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        if self._isUser(update):
+            reply_keyboard = []
+            reply_keyboard.append(["zeige aktuelles Modell"])
+            reply_keyboard.append(["Modell wählen"])
+            try:
+                await update.message.reply_text("Wähle?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+            except error.NetworkError:
+                self.updater = Application.builder().token(self.config.telegram_token).build()
+                return self.model(update, context)
+        return self.MODELSELECT
+    
+    async def showmodel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        try:
+            await update.message.reply_text("Das aktuell verwendete Modell: {}".format(self.config.current_model), reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.showmodel(update, context)
+        return ConversationHandler.END
+    
+    async def setmodel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # create selection of models to choose from ..
+        available_models = self.openai_api.getAvailableModels()
+        reply_keyboard = []
+        for model in available_models:
+            reply_keyboard.append([model])
+        try:
+            await update.message.reply_text("Verfügbare Modelle:", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.setmodel(update, context)
+        return self.MODELSELECTED
 
-    async def topicname(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        await update.message.reply_text("not implemented yet")
+    async def setnewmodel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # set the new model for this bot
+        self.openai_api.setModel(update.message.text)
+        try:
+            await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.setnewmodel(update, context)
         return ConversationHandler.END
 
     async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        await update.message.reply_text("OK")
+        try:
+            await update.message.reply_text("OK", reply_markup= ReplyKeyboardRemove())
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.cancel(update, context)
+        return ConversationHandler.END
+
+    async def start(self, update, context):
+        try:
+            if self._isUser(update):
+                await update.message.reply_text("Wilkommen beim ChatGPT Telegram Bot!")
+            else:
+                await update.message.reply_text("You are not in the valid users list!")
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.start(update, context)
+
+    async def help(self, update, context):
+        try:
+            if self._isUser(update):
+                await update.message.reply_text("Wilkommen beim ChatGPT Telegram Bot!")
+            else:
+                await update.message.reply_text("You are not in the valid users list!")
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.help(update, context)
+
+    async def chat_query(self, update, context):
+        try:
+            if self._isUser(update):
+                # filter commands
+                if update.message.text == "/cancel":
+                    await update.message.reply_text("OK")
+                    return ConversationHandler.END
+                elif update.message.text[0] == "/":
+                    return self.CHAT
+                # chat with or without history..
+                else:
+                    user = self.userById(update)
+                    response = ""
+                    if user.hasActiveTopic(): # chat with active topic
+                        currentTopic = user.data['current_topic']
+                        currentHistory = user.historyOfTopic(currentTopic)
+                        currentHistory.append(update.message.text)
+                        new_query_with_history = "\n".join(currentHistory)
+                        self.openai_api.setQueryText(new_query_with_history)
+                        response = self.openai_api.getResponse()
+                        currentHistory.append(response)
+                        user.updateHistory(currentTopic, currentHistory)
+                        user.save()
+                    else: # chat without topic
+                        self.openai_api.setQueryText(update.message.text)
+                        response = self.openai_api.getResponse()
+                    if response != "":
+                        await update.message.reply_text(response)
+                    return self.CHAT
+            else:
+                await update.message.reply_text("You are not in the valid users list!")
+        except error.NetworkError:
+            self.updater = Application.builder().token(self.config.telegram_token).build()
+            return self.chat_query(update, context)
         return ConversationHandler.END
 
     def run(self) -> None:
@@ -267,55 +445,11 @@ class ChatGPTBot:
             userId = str(update.message.from_user.id)
             if user.id == userId:
                 return user
-    
-    async def start(self, update, context):
-        if self._isUser(update):
-            await update.message.reply_text("Wilkommen beim ChatGPT Telegram Bot!")
-        else:
-            await update.message.reply_text("You are not in the valid users list!")
-
-    async def help(self, update, context):
-        if self._isUser(update):
-            await update.message.reply_text("Wilkommen beim ChatGPT Telegram Bot!")
-        else:
-            await update.message.reply_text("You are not in the valid users list!")
-
-    async def chat_query(self, update, context):
-        if self._isUser(update):
-            # filter commands
-            if update.message.text == "/cancel":
-                await update.message.reply_text("OK")
-                return ConversationHandler.END
-            elif update.message.text[0] == "/":
-                return self.CHAT
-            # chat with or without history..
-            else:
-                user = self.userById(update)
-                response = ""
-                if user.hasActiveTopic(): # chat with active topic
-                    currentTopic = user.data['current_topic']
-                    currentHistory = user.historyOfTopic(currentTopic)
-                    currentHistory.append(update.message.text)
-                    new_query_with_history = "\n".join(currentHistory)
-                    openai_api.setQueryText(new_query_with_history)
-                    response = openai_api.getResponse()
-                    currentHistory.append(response)
-                    user.updateHistory(currentTopic, currentHistory)
-                    user.save()
-                else: # chat without topic
-                    openai_api.setQueryText(update.message.text)
-                    response = openai_api.getResponse()
-                if response != "":
-                    await update.message.reply_text(response)
-                return self.CHAT
-        else:
-            await update.message.reply_text("You are not in the valid users list!")
-        return ConversationHandler.END
 
 # -------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     config = Config()
-    openai_api = OpenaAI_API(config.openai_key)
+    openai_api = OpenaAI_API(config)
     chatGPT_bot = ChatGPTBot(config, openai_api)
     chatGPT_bot.run()
